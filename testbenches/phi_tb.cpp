05e1dfc81f10c9aecc6fb1ccdf569bd33cd4912f
liuutin9 <136707519+liuutin9@users.noreply.github.com>
2025-07-03 00:06:23 +0800

    Update phi_tb.cpp

--- testbenches/phi_tb.cpp
+++ testbenches/phi_tb.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 // #include "../modules/phi-2/phi-2_config.hpp"
 
-// void PhiForCausalLM_forward(
-//     float out_logits[SLEN][VOCAB_SIZE],
-//     float out_past_key_values[NUM_HIDDEN_LAYERS][NUM_ATTENTION_HEADS][SLEN][HEAD_DIM],
-//     int input_ids[SLEN],
-//     float past_key_values[NUM_HIDDEN_LAYERS][NUM_ATTENTION_HEADS][SLEN][HEAD_DIM] // cache
-// );
+#define HEAD_DIM 80 // HIDDEN_SIZE / NUM_ATTENTION_HEADS
+#define NUM_ATTENTION_HEADS 32
+#define NUM_HIDDEN_LAYERS 32
+#define SLEN 2048 // 最大序列長度
+#define VOCAB_SIZE 51200
+
+void PhiForCausalLM_forward(
+    float out_logits[SLEN][VOCAB_SIZE],
+    float out_past_key_values[NUM_HIDDEN_LAYERS][NUM_ATTENTION_HEADS][SLEN][HEAD_DIM],
+    int input_ids[SLEN],
+    float past_key_values[NUM_HIDDEN_LAYERS][NUM_ATTENTION_HEADS][SLEN][HEAD_DIM] // cache
+);
 
 int main() {
     std::cout << "hello world" << '\n';
